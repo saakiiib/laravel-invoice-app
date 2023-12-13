3541379c070b21d1617347bf89cafa4b4d8490d8
Sakib <nsakib.miraz@gmail.com>
2023-12-13 12:15:06 +0600

    Model, migration, factory, seeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -3,6 +3,11 @@
 namespace Database\Seeders;
 
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use App\Models\Counter;
+use App\Models\Customer;
+use App\Models\Invoice;
+use App\Models\InvoiceItem;
+use App\Models\Product;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
@@ -12,11 +17,10 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        // \App\Models\User::factory(10)->create();
-
-        // \App\Models\User::factory()->create([
-        //     'name' => 'Test User',
-        //     'email' => 'test@example.com',
-        // ]);
+        // Counter::factory(10)->create();
+        // Product::factory(10)->create();
+        // Customer::factory(10)->create();
+        // Invoice::factory(10)->create();
+        InvoiceItem::factory(10)->create();
     }
 }
